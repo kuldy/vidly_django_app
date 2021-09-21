704a033ad5e6a183c6416b4ba0929cfbfb940253
kuldeep <kuldykld@gmail.com>
2021-09-21 19:02:38 +0530

    tasks related to movies app

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 
 # Register your models here.
+from . models import Genre, Movie
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'release_year', 'daily_rate')
+    exclude = ('date_created', )
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
