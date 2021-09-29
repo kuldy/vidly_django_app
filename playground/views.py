9fd6e5d2b9dc8b56eaa24f348687c8b6bd1e22a8
kuldeep <kuldykld@gmail.com>
2021-09-29 20:56:06 +0530

    managers and query_set

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 # Create your views here.
 
 
 def say_hello(request):
+    # query_set = Product.objects.all()
+    # for product in query_set:
+    #     print(product)
+    # list(query_set)
+    # query_set[0]
+
     return render(request, 'hello.html', {'kullu': 'Kuldeep Singh'})
