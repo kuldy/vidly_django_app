0784c037c4fb138c31fbcc26626001ebd19e686f
kuldeep <kuldykld@gmail.com>
2021-09-30 20:25:57 +0530

    complex lookup using Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 
 # Create your views here.
@@ -23,7 +24,7 @@ def say_hello(request):
     # product = Product.objects.filter(pk=0).first()
 
     # Filtering products/ search queryset api to find all type of lookup sets
-    query_set = Product.objects.filter(unit_price__gt=200)
+    # query_set = Product.objects.filter(unit_price__gt=200)
     # query_set = Product.objects.filter(unit_price__range=(300, 600))
     # query_set = Product.objects.filter(collection__id__range=(1, 2))
     # query_set = Product.objects.filter(title__contains='Cat')
@@ -31,5 +32,12 @@ def say_hello(request):
     # query_set = Product.objects.filter(last_updated__year=2021)
     # query_set = Product.objects.filter(last_updated__date='2021-09-02')
     # query_set = Product.objects.filter(description__isnull=True)
+
+    # query_set = Product.objects.filter(inventory__lt=20, unit_price__lt=300)
+    # query_set = Product.objects.filter(inventory__lt=20).filter(unit_price__lt=300)
+    # query_set = Product.objects.filter(
+    #     Q(inventory__lt=20) | Q(unit_price__lt=401))
+    query_set = Product.objects.filter(
+        Q(inventory__lt=20) | ~Q(unit_price__lt=401))
 
     return render(request, 'hello.html', {'kullu': 'Kuldeep Singh', 'products': list(query_set)})
