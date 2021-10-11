1615070553203c398cbfcc8d8fb2934f9ce12b37
kuldeep <kuldykld@gmail.com>
2021-10-11 20:54:11 +0530

    creating api Views

--- vidly/urls.py
+++ vidly/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path('movies/', include('movies.urls')),
     path('api/', include(movie_resource.urls)),
     path('playground/', include('playground.urls')),
+    path('store/', include('store.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
